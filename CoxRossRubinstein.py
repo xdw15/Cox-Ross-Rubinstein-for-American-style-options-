d56354aed5e054c58692d4dcd87f7c9a9f3f9577
xdw15 <30297811+xdw15@users.noreply.github.com>
2020-10-01 03:49:52 -0500

    Update CoxRossRubinstein.py

--- CoxRossRubinstein.py
+++ CoxRossRubinstein.py
@@ -1,11 +1,22 @@
 # Cox-Ross-Rubinstein for Eurpean and American style options
 
-#Code is straightorward and only numpy is required
+#Code is straightforward and only numpy is required
 
 
 import numpy as np
 
-def crr(S,K,sigma,r,T,steps,q,call=True,american=True): 
+#S        -> Spot price of the underlying
+#K        -> Strike price for the option
+#sigma    -> Underlying volatility annualized
+#r        -> continously compounded risk-free rate
+#T        -> option's time to expiration in years
+#steps    -> number of steps to be calculated in the tree
+#q        -> continously compounded dividend yield (default is 0)
+#call     -> specify put or call option (default is call)
+#american -> specify if early exercise (default is true, false for European style)
+
+
+def crr(S,K,sigma,r,T,steps,q=0,call=True,american=True): 
     # Assigning paramter values
     dt=T/steps
     u=np.exp(dt**0.5 *sigma)
@@ -16,12 +27,12 @@ def crr(S,K,sigma,r,T,steps,q,call=True,american=True):
     pricetree=np.zeros([steps+1,steps+1],dtype='float64')
     pricetree[0,0]=S
     
-    #The tree is calculated
+    #The tree is calculated for the underlying's price
     for i in range(1,steps+1):
        pricetree[:i,i]=pricetree[:i,i-1]*u
        pricetree[i,i]=pricetree[i-1,i-1]*d
        
-    #Same process for the option tree 
+    #Tree for the option value
     optiontree=np.zeros([steps+1,steps+1],dtype='float64')
     if call:
         optiontree[:,steps]=np.maximum(pricetree[:,-1 ]-K,0)
@@ -31,7 +42,7 @@ def crr(S,K,sigma,r,T,steps,q,call=True,american=True):
     for i in range(steps-1,-1,-1):
         optiontree[:i+1,i]=np.exp(-(r*dt))*(optiontree[:i+1,i+1]*p+\
                                             (1-p)*optiontree[1:i+2:1,i+1])
-        #Early excersie    
+        #Early exercise    
         if american :
             if call :
                 optiontree[:i+1,i]=np.maximum( pricetree[:i+1,i]-K, \
@@ -40,27 +51,24 @@ def crr(S,K,sigma,r,T,steps,q,call=True,american=True):
                 optiontree[:i+1,i]=np.maximum(K-pricetree[:i+1,i], \
                                               optiontree[:i+1,i])
                     
-    #Finally, the function outputs the options value, and the price and 
-    # option value arrays                
+    #Finally, the function outputs the option value, and the price and 
+    # option value trees.              
     return optiontree[0,0], pricetree, optiontree
     
 
 
                                         
-##### Testing the function ######
+##### Using the function with some given parameters ######
     
 S=50;
 K=30;
 sigma= 0.3;
 r=0.05;
-q=0;
 T=1;
 steps=200;
-american=True
-call=True
 
 
-price,pricearray,optionarray=crr(S,K,sigma,r,T,steps,q,True,True)
+price,pricearray,optionarray=crr(S,K,sigma,r,T,steps)
 
 
     
